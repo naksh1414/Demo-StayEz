58f7d3d5ef91063e519487c8b253a3d757e4c9c8
Nakshatra Manglik <nakshatramanglik14@gmail.com>
2024-07-24 04:15:29 +0530

    Stayex web

--- src/Pages/Flats/Flats.jsx
+++ src/Pages/Flats/Flats.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import Card from "../../Components/Cards/Card";
 import MapImg from "../../assets/Map.png";
 import data from "../../data/properies.json";
@@ -13,20 +13,20 @@ import GenderModal from "../../Components/Modal/GenderModal";
 const Flats = () => {
   const [priceModalIsOpen, setPriceModalIsOpen] = useState(false);
   const [distanceModalIsOpen, setDistanceModalIsOpen] = useState(false);
-  const [selectedPriceRange, setSelectedPriceRange] = useState([
-    6000,
-    Infinity,
-  ]);
+  const [selectedPriceRange, setSelectedPriceRange] = useState([6000, Infinity]);
   const [selectedDistance, setSelectedDistance] = useState(Infinity);
   const [genderModalIsOpen, setGenderModalIsOpen] = useState(false);
+  const [selectedGender, setSelectedGender] = useState("both");
+  const [wishlist, setWishlist] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 3;
+
   const openPriceModal = () => setPriceModalIsOpen(true);
   const closePriceModal = () => setPriceModalIsOpen(false);
   const openDistanceModal = () => setDistanceModalIsOpen(true);
   const closeDistanceModal = () => setDistanceModalIsOpen(false);
   const openGenderModal = () => setGenderModalIsOpen(true);
   const closeGenderModal = () => setGenderModalIsOpen(false);
-  const [selectedGender, setSelectedGender] = useState("both");
-  const [wishlist, setWishlist] = useState([]);
 
   const handleApplyPriceRange = (range) => {
     setSelectedPriceRange(range);
@@ -37,14 +37,16 @@ const Flats = () => {
     setSelectedDistance(distance);
     closeDistanceModal();
   };
+
+  const handleApplyGender = (gender) => {
+    setSelectedGender(gender);
+    closeGenderModal();
+  };
+
   const handleRemoveFilters = () => {
     setSelectedPriceRange([6000, Infinity]);
     setSelectedDistance(Infinity);
     setSelectedGender("both");
-  };
-  const handleApplyGender = (gender) => {
-    setSelectedGender(gender);
-    closeGenderModal();
   };
 
   const handleRemovePriceFilter = () => {
@@ -75,27 +77,6 @@ const Flats = () => {
     localStorage.setItem("Wishlist", JSON.stringify(newWishlist));
   };
 
-  // const handleAddToWishlist = (property) => {
-  //   const savedWishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
-  //   const updatedWishlist = [...savedWishlist, property];
-  //   localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
-  //   setWishlist(updatedWishlist);
-  // };
-
-  // useEffect(() => {
-  //   // Load wishlist items from local storage
-  //   const savedWishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
-  //   setWishlist(savedWishlist);
-  // }, []);
-
-  // const handleAddToWishlist = useCallback((property) => {
-  //   setWishlist((prevWishlist) => {
-  //     const updatedWishlist = [...prevWishlist, property];
-  //     localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
-  //     return updatedWishlist;
-  //   });
-  // }, []);
-
   const filteredData = data.filter((property) => {
     const matchesPrice =
       property.starting_price >= selectedPriceRange[0] &&
@@ -107,9 +88,19 @@ const Flats = () => {
 
     return matchesPrice && matchesDistance && matchesGender;
   });
-  console.log("gender", selectedGender);
-  console.log("Filtered Data:", filteredData);
-  console.log(data);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const indexOfLastProperty = currentPage * itemsPerPage;
+  const indexOfFirstProperty = indexOfLastProperty - itemsPerPage;
+  const currentProperties = filteredData.slice(
+    indexOfFirstProperty,
+    indexOfLastProperty
+  );
+
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
 
   return (
     <main>
@@ -190,23 +181,32 @@ const Flats = () => {
       <div className="flex flex-row justify-center mt-[150px] md:mt-5">
         <div className="p-5 justify-center items-center flex flex-col">
           <div>
-            {filteredData.length > 0 ? (
-              filteredData.map((property) => (
-                <Card
-                  key={property.id}
-                  property={property}
-                  addToWishList={() => handleAddToWishlist(property)}
-                />
-              ))
-            ) : (
-              <p className="text-white text-center w-[700px] font-semibold">
-                No properties found.
-              </p>
-            )}
+            {currentProperties.map((property, index) => (
+              <Card
+                key={index}
+                property={property}
+                onAddToWishlist={() => handleAddToWishlist(property)}
+              />
+            ))}
+          </div>
+          <div className="flex flex-wrap space-y-2 md:space-y-0 justify-center space-x-2 mt-10">
+            {Array.from({ length: totalPages }, (_, index) => (
+              <button
+                key={index}
+                onClick={() => handlePageChange(index + 1)}
+                className={`px-4 py-2 rounded-md ${
+                  currentPage === index + 1
+                    ? "bg-[#2590ce] border-2 text-white"
+                    : "bg-none border-2 text-white"
+                }`}
+              >
+                {index + 1}
+              </button>
+            ))}
           </div>
         </div>
         <div className="hidden md:flex mt-20 ml-20 p-5">
-          <img className="h-[600px] w-[80%]" src={MapImg} alt="image" />
+          <img className="h-[800px] w-[80%] mt-10" src={MapImg} alt="image" />
         </div>
       </div>
     </main>
