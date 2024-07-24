3bb46db30530ec30aac3b94eb95af1f7821b0fd8
Nakshatra Manglik <nakshatramanglik14@gmail.com>
2024-07-24 11:55:15 +0530

    Stayex web

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,64 @@
-import FImg from "../../assets/Footer.png"
+import { useState } from "react";
+import FImg from "../../assets/Footer.png";
+import Logo from "../../assets/FooterLogo.png";
+import BtnImg from "../../assets/Play stote and app store icon for website .png";
+import { FaTwitter } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 const Footer = () => {
+  const [email, setEmail] = useState("");
   return (
-    <main className="flex justify-center items-center w-full">
-      <img className="object-cover w-full" src={FImg} alt="" />
+    <main className="bg-[#152038] flex-col flex justify-center items-center w-full h-[400px] px-3">
+      {/* <img className="object-cover w-full" src={FImg} alt="" /> */}
+      <div className="text-white text-[1.4rem] font-semibold flex justify-around w-[80%] mt-5">
+        <h1>Help</h1>
+        <h1>About</h1>
+        <h1>Company</h1>
+        <h1>Services</h1>
+        <h1>Contact Us</h1>
+      </div>
+      <div className="mt-10 flex flex-row items-center justify-between w-full">
+        <div className="pl-5">
+          <img className="h-[160px] w-[180px]" src={Logo} alt="" />
+        </div>
+        <div className="mt-20">
+          <img src={BtnImg} alt="" />
+        </div>
+        <div className="text-white font-semibold text-[1rem] mt-[-30px] pr-5 items-center justify-center flex flex-col">
+          <h1 className="text-[1.3rem]">Subscribe For Updates </h1>
+          <div className="mt-2">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-white/30 placeholder:text-white px-5 py-3 rounded-tl-2xl rounded-bl-2xl"
+              placeholder="Your Mail"
+            />
+            <button className="text-[#03A9F4] bg-white px-5 py-3 rounded-tr-2xl rounded-br-2xl">
+              Subscribe
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="border-t-2 w-full mt-10 flex items-center justify-between">
+        <div className="flex justify-between font-semibold space-x-3 w-[200px] mt-3 text-white">
+          <h1>Terms</h1>
+          <h1>Conditions</h1>
+          <h1>Privacy</h1>
+        </div>
+        <div>
+          <p className="text-white mt-3">@ 2024 StayEz , ALL RIGHTS RESERVED</p>
+        </div>
+        <div className="text-white flex space-x-8 mt-3">
+          <FaTwitter className="h-8 w-8"></FaTwitter>
+          <FaYoutube className="h-8 w-8"></FaYoutube>
+          <FaFacebook className="h-8 w-8"></FaFacebook>
+          <FaInstagram className="h-8 w-8"></FaInstagram>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
